2519b31bb7d55e0ba70f3ed980fdfb56fd880d95
Xiaoge Zhang <xiaogezhang@Xiaoges-MacBook-Pro.local>
2020-02-11 23:12:02 -0500

    fixed gaussian function & plot

--- gaussian_vec.m
+++ gaussian_vec.m
@@ -1,11 +1,18 @@
 function [y] = gaussian_vec(x, varargin)
-  mu = 0;
-  sig2 = 1;
+%  mu = 0;
+%  sig2 = 1;
   try
     mu = varargin{1};
-    sig2 = varargin{2};
+  catch
+     mu = 0;
   end
-  y = 1/(sqrt(sig2*2*pi))*exp(-1/2*square(x-mu)/sig2)
+  try
+    sig2 = varargin{2};
+  catch
+    sig2 = 1;
+  end
+  
+  y = 1/(sqrt(sig2*2*pi))*exp(-1/2*(x-mu).^2/sig2);
 end
 
 %fx1 = @(x, mu, sig2) 1/(sqrt(sig2*2*pi))*exp(-1/2*square(x-mu)/sig2);
