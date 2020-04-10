b9016308772402d5199e998d1834e1e912ec62f6
Cole Turner <coleski1@gmail.com>
2020-04-10 14:33:15 -0700

    setting up the cq net

---
+++ Project3/cq_net.py
@@ -0,0 +1,25 @@
+'''cq_net.py
+Implements the CQ network
+CS443: Computational Neuroscience
+Alice Cole Ethan
+Project 3: Competitive Networks
+'''
+import numpy as np
+
+
+def rcf_linear():
+    """This is a linear layer(xi)"""
+    pass
+
+def rcf_wta():
+    """This is a winner take all layer (yi)"""
+    pass
+
+def leaky():
+    """ This is the leaky integrator wi layer."""
+    pass
+
+
+def competitive_queue():
+    """This puts together all the layers"""
+    pass
